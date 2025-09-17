7ad4b81bd6ea2d9d1b909bf7d3ed3a46466c5741
sandistwnn <sandisetiawan3100@gmail.com>
2025-09-17 22:20:30 +0700

    feature: add anggota information to package.json and implement greeting and aboutUsRoutes

--- routes/greetingRoute.js
+++ routes/greetingRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // GET /getting - API information
 router.get('/', (req, res) => {
-    // IMPLEMENTASI DISINI
+    res.json({ message: 'Selamat datang di API kami!' });
 });
 
 module.exports = router;
