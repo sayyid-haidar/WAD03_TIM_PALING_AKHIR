e0f30f04ecc421871c18496a9e940748a3471610
sandistwnn <sandisetiawan3100@gmail.com>
2025-10-18 21:57:38 +0700

    fix users

--- src/routes/aboutUsRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const aboutUsController = require('../controllers/aboutUsController');
-
-router.get('/:name', aboutUsController.getAnggotaByName);
-
-module.exports = router;
--- src/routes/greetingRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const greetingController = require('../controllers/greetingController');
-
-router.get("/", greetingController.getWelcome);
-
-router.get("/greeting", greetingController.getGreeting);
-
-module.exports = router;
