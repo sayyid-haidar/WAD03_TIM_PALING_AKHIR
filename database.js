9f09bb79d01e322133461e8a22fc5852d5c7d618
sandistwnn <sandisetiawan3100@gmail.com>
2025-10-19 20:56:50 +0700

    Refactor: Implement Sequelize for User feature

--- database.js
+++ database.js
@@ -1,13 +1,20 @@
-const { Pool } = require('pg');
+const { Sequelize } = require('sequelize');
+const path = require('path');
 
-const pool = new Pool({
-    user: 'postgres',       // username database
+// 1. Konfigurasi koneksi Sequelize
+const sequelize = new Sequelize('postgresWAD', 'postgres', 'kataguaa', {
     host: 'localhost',
-    database: 'postgresWAD', // nama database
-    password: 'kataguaa',  // password database
-    port: 5432,
+    dialect: 'postgres'
 });
 
-module.exports = {
-    query: (text, params) => pool.query(text, params),
-};
+const db = {};
+
+// 2. Masukkan instance Sequelize dan class Sequelize ke object db
+db.Sequelize = Sequelize; // Menyimpan library Sequelize itu sendiri untuk mengakses tipe data dan alat lainnya.
+db.sequelize = sequelize; // Menyimpan koneksi aktif ke database.
+
+// 3. Muat model User dan hubungkan dengan instance sequelize
+db.users = require(path.join(__dirname, 'src/models/user.js'))(sequelize, Sequelize);
+// (Jika ada model lain, tambahkan di sini)
+
+module.exports = db;
